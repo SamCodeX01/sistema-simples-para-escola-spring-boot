6502619bfd2be4ec588937c3cb362a5356d2b154
SamCodeX01 <txtstring@gmail.com>
2025-12-12 16:55:47 -0300

    Atualizar

--- src/main/java/sistema/escola/br/com/Sistema/para/Escola/model/Aluno.java
+++ src/main/java/sistema/escola/br/com/Sistema/para/Escola/model/Aluno.java
@@ -4,7 +4,9 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Data;
 
+@Data //Aqui já esta tratando o Getters e Setters
 @Table //Identifica que essa classe é uma tabela
 @Entity //Identifica que essa classe é uma entidade
 public class Aluno {
@@ -19,4 +21,17 @@ public class Aluno {
         private  float altura;
     @Column
         private String curso;
+
+
+    public Aluno(int RA, String nome, int idade, float altura, String curso) {
+        this.RA = RA;
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+        this.curso = curso;
+    }
+
+    public Aluno() {
+    }
 }
+
